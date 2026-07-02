9f4d56bb88c72c50fddfd723d870319e392c9a0d
Parthwebde12 <parthwakodikar4@gmail.com>
2026-07-02 09:17:32 +0530

    Switch CLI to call Flask API instead of reading files

--- cli/commands/risk.js
+++ cli/commands/risk.js
@@ -1,18 +1,10 @@
-const fs = require('fs');
-const path = require('path');
+const axios = require('axios');
 
-module.exports = function (district) {
-  const filePath = path.join(__dirname, '../../data/risk_predictions.json');
-
-  if (!fs.existsSync(filePath)) {
-    console.log('Data file not found.');
-    return;
-  }
-
-  const data = JSON.parse(fs.readFileSync(filePath, 'utf-8'));
-  if (data.district && data.district.toLowerCase() === district.toLowerCase()) {
-    console.log(`Risk for ${data.district}: ${data.risk} (score: ${data.score})`);
-  } else {
-    console.log(`No data found for "${district}".`);
+module.exports = async function (district) {
+  try {
+    const res = await axios.get(`http://127.0.0.1:5000/hello`);
+    console.log(res.data.message);
+  } catch (err) {
+    console.log('Could not reach the crime API. Is it running?');
   }
 };
